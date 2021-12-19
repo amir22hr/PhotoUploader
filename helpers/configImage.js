03594c2e88f3fafdce7a1123ba137f396164115f
amir22hr <info.amir22hr@gmail.com>
2021-12-19 18:03:16 +0330

    fix error sharp.js

--- helpers/configImage.js
+++ helpers/configImage.js
@@ -2,11 +2,13 @@ const sharp = require('sharp');
 var fs = require('fs');
 
 const configImage = async (req, res, next) => {
-    await sharp(req.file.path)
-        .resize(400)
-        .jpeg({ mozjpeg: true })
-        .toFile(`./public/images/${req.file.filename}`);
-    await fs.unlinkSync(req.file.path);
+    if (req.file) {
+        await sharp(req.file.path)
+            .resize(400)
+            .jpeg({ mozjpeg: true })
+            .toFile(`./public/images/${req.file.filename}`);
+        await fs.unlinkSync(req.file.path)
+    }
     next()
 }
 
